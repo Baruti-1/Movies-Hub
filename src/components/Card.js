ceee0f67c78bd8cc13493de9b42a7e1a2689e0a2
Baruti-1 <barutibaruani1@gmail.com>
2023-04-25 21:59:07 +0300

    Create footer component

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ export const Card = () => {
       <div className="p-5">
         <Link to="#">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
+            Noteworthy technology acquisitions 2024
           </h5>
         </Link>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
