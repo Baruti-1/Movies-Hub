ebd9102ad118d7671a7c155054fc354c8ab7ad53
Baruti-1 <barutibaruani1@gmail.com>
2023-04-29 16:58:51 +0300

    Implement movie detail page

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,7 +39,7 @@ export const Header = () => {
           <Link to="/" className="flex items-center">
             <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Cinemate Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Cinemate
+              Movies Hub
             </span>
           </Link>
 
